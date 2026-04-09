9def9cc6ff424c8e257b31c8c65386f4c2af4d3f
vyjayanthik <148877749+vyjayanthik@users.noreply.github.com>
2026-04-09 12:46:12 +0100

    Update churn_queries.sql

--- sql/churn_queries.sql
+++ sql/churn_queries.sql
@@ -1,2 +1,61 @@
--- Churn rate by contract
-SELECT Contract, COUNT(*) ...
+CREATE DATABASE IF NOT EXISTS churn_analysis;
+USE churn_analysis;
+SHOW DATABASES;
+CREATE TABLE customers (
+    customerID VARCHAR(50) PRIMARY KEY,
+    gender VARCHAR(10),
+    SeniorCitizen INT,
+    Partner VARCHAR(10),
+    Dependents VARCHAR(10),
+    tenure INT,
+    PhoneService VARCHAR(10),
+    MultipleLines VARCHAR(20),
+    InternetService VARCHAR(20),
+    OnlineSecurity VARCHAR(20),
+    OnlineBackup VARCHAR(20),
+    DeviceProtection VARCHAR(20),
+    TechSupport VARCHAR(20),
+    StreamingTV VARCHAR(20),
+    StreamingMovies VARCHAR(20),
+    Contract VARCHAR(30),
+    PaperlessBilling VARCHAR(10),
+    PaymentMethod VARCHAR(50),
+    MonthlyCharges DECIMAL(10,2),
+    TotalCharges DECIMAL(10,2),
+    Churn VARCHAR(10)
+);
+SHOW TABLES;
+DESCRIBE customers;
+SELECT COUNT(*) AS total_records FROM customers;
+SELECT * FROM customers LIMIT 10;
+UPDATE customers
+SET TotalCharges = 0
+WHERE TotalCharges IS NULL;
+ALTER TABLE customers
+MODIFY COLUMN TotalCharges DECIMAL(10,2);
+SELECT 
+    COUNT(*) AS total_customers,
+    SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) AS churned_customers,
+    ROUND(
+        SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 
+        2
+    ) AS churn_rate_percentage
+FROM customers;
+SELECT 
+    Contract,
+    COUNT(*) AS total_customers,
+    SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) AS churned_customers,
+    ROUND(
+        SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 
+        2
+    ) AS churn_rate_percentage
+FROM customers
+GROUP BY Contract
+ORDER BY churn_rate_percentage DESC;
+SELECT 
+    Churn,
+    ROUND(AVG(MonthlyCharges), 2) AS avg_monthly_charges
+FROM customers
+GROUP BY Churn;
+
+SELECT USER();
